7c23c1cade5d1dd2b6edbae1b24c1da62a2c7fea
BrayanMoraH <153476683+BrayanMoraH@users.noreply.github.com>
2026-04-23 22:21:07 -0600

    Add: is-anagram - submission-16

---
+++ Data Structures & Algorithms/is-anagram/submission-16.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_sorted = "".join(sorted(s))
+        t_sorted = "".join(sorted(t))
+
+        print(s_sorted)
+        print(t_sorted)
+
+        return s_sorted == t_sorted
+        
