5c7a537670eb433224285f1a5958d991c8f5ab52
BrayanMoraH <153476683+BrayanMoraH@users.noreply.github.com>
2026-05-04 20:15:09 -0600

    Add: time-based-key-value-store - submission-13

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-13.py
@@ -0,0 +1,44 @@
+class TimeMap:
+
+    def __init__(self):
+        self.key_value = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.key_value[key].append([value, timestamp])
+
+    def get(self, key: str, timestamp: int) -> str:
+
+        current = self.key_value.get(key, [])
+
+        result = ""
+
+        if len(current) == 0:
+            return result
+        
+        left = 0
+        right = len(current) - 1
+
+        while(left <= right):
+
+            middle = (left + right) // 2
+
+            if current[middle][1] == timestamp:
+                return current[middle][0]
+
+            if current[middle][1] <= timestamp:
+                result = current[middle][0]
+
+            if current[middle][1] > timestamp:
+                right = middle - 1
+            else:
+                left = middle + 1
+            
+        return result
+            
+
+
+        
+
+
+
+        
